123354355d63187676ec6e308a8567a832fa96f3
Katalmach <katalmach7@gmail.com>
2021-06-16 16:11:43 +0200

    Penultimo commit 2

--- museoVonwillerFarnese/src/main/java/it/uniroma3/siw/MuseoVonwillerApplication.java
+++ museoVonwillerFarnese/src/main/java/it/uniroma3/siw/MuseoVonwillerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MuseoArtiDelNordApplication {
+public class MuseoVonwillerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MuseoArtiDelNordApplication.class, args);
+		SpringApplication.run(MuseoVonwillerApplication.class, args);
 	}
 
 }
